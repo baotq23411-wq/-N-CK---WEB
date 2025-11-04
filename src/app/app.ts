7da26d3b1a78f32458d24dff9c28aa578b08b00e
Pruby05 <hongngocmocay@gmail.com>
2025-11-05 00:19:34 +0700

    add icon bootstrap

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { RoomList } from './room-list/room-list';
 import { FormsModule } from '@angular/forms';
-import { RouterModule, RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule, FormsModule],
+  standalone: true,
+  imports: [RouterOutlet, RoomList, CommonModule, FormsModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class App {
   protected readonly title = signal('panacea');
