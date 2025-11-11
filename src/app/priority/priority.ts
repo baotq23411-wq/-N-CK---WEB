bf96f2d38307c0f2c715ccf1efbf966bebc08e16
hoangthailuong <thailh23411@st.uel.edu.vn>
2025-11-11 14:14:13 +0700

    priority

---
+++ src/app/priority/priority.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-priority',
+  imports: [],
+  templateUrl: './priority.html',
+  styleUrl: './priority.css',
+})
+export class Priority {
+
+}
